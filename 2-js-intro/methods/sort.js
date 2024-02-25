77f3231597c69f45e30f1a2dc5bcafd61d9be587
Hila Gvar <hilla.gvar@gmail.com>
2024-02-25 16:11:50 +0100

    sort homework

--- 2-js-intro/methods/sort.js
+++ 2-js-intro/methods/sort.js
@@ -39,20 +39,72 @@ const students3 = [
     { name: "Ona", age: 66, isMarried: true, marks: [7, 7, 7] },
 ];
 
+console.log("---------");
+
 // reikia apskaiciuoti kiekvieno studento pazymiu kieki ir tai prideti kaip
 // nauja studento objekto rakta (marksCount)
+
+for (const student of students3) {
+    student.marksCount = student.marks.length;
+}
+
+console.log(students3);
+console.log("---------");
 
 // reikia apskaiciuoti kiekvieno studento pazymiu vidurki ir tai prideti
 // kaip nauja studento objekto rakta (marksAverage)
 
+for (const student of students3) {
+    let marksSum = 0;
+    for (const mark of student.marks) {
+        marksSum += mark;
+    }
+    student.marksAverage = marksSum / student.marksCount;
+}
+
+console.log(students3);
+console.log("---------");
+
 // reikia rasti kiekvieno studento maziausia pazymi ir tai prideti kaip
-// nauja studento objekto rakta (minMark)
+// nauja studento objekto rakta (minMark) 
+
+for (const student of students3) {
+    const sortedMarks = student.marks.toSorted((a, b) => a - b);
+    // student.marks.sort((a, b) => a - b);
+    student.minMark = sortedMarks[0];
+}
+
+console.log(students3);
+console.log("---------");
 
 // reikia rasti kiekvieno studento didziausia pazymi ir tai prideti kaip
 // nauja studento objekto rakta (maxMark)
 
+for (const student of students3) {
+    const sortedMarks = student.marks.toSorted((a, b) => b - a);
+    student.maxMark = sortedMarks[0];
+}
+
+console.log(students3);
+console.log("---------");
+
 // reikia sudaryti studentu sarasa (pilni objektai), kuriu pazymiu
 // vidurkis yra didesnis uz 5
 
+const goodStudents = students3.filter(student => student.marksAverage > 5);
+
+console.log(goodStudents);
+console.log("---------");
+
 // reikia sudaryti studentu vardu sarasa, kuriu pazymiu vidurkis yra
 // didesnis uz 5
+
+const goodStudentNames = [];
+
+for (const student of students3) {
+    if (student.marksAverage > 5) {
+        goodStudentNames.push(student.name);
+    }
+}
+
+console.log(goodStudentNames);
